8bdc8ba132c05486d414e48f1badfadb8d73e30d
Jason Wong <wongjas@gmail.com>
2015-11-23 21:30:31 -0800

    change database to mysql

--- jblog/jblog/settings.py
+++ jblog/jblog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'default',
+        'HOST': 'wongjas.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
     }
 }
 
