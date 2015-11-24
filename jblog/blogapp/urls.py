586a4bdb6adbc492530288f77ec9785286a296e0
Jason Wong <wongjas@gmail.com>
2015-11-23 21:09:59 -0800

    Add navbar and main page links

--- jblog/blogapp/urls.py
+++ jblog/blogapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.main_page, name='main_page'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^blog/$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- jblog/blogapp/views.py
+++ jblog/blogapp/views.py
@@ -10,3 +10,9 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blogapp/post_detail.html', {'post': post})
+
+def main_page(request):
+	return render(request, 'blogapp/main_page.html', {})
+
+def about(request):
+	return render(request, 'blogapp/about.html', {})
